89a43f9e657037f21096488bab131be02cd88fef
Oscar Reyes <>
2016-11-16 21:22:06 -0600

    Added variable declarations and constructor to Schedule class

--- src/scheduleCreator/model/Schedule.java
+++ src/scheduleCreator/model/Schedule.java
@@ -1,5 +1,15 @@
 package scheduleCreator.model;
 
-public class Schedule {
-
+public class Schedule 
+{
+	private final String classDays;
+	private final String classHours;
+	private final String classLocation;
+	
+	public Schedule(final String days, final String hours, final String location)
+	{
+		this.classDays = days;
+		this.classHours = hours;
+		this.classLocation = location;
+	}
 }
