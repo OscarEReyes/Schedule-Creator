275e4ec9426ec7d2862b5466b5bbbbfeb33be9bd
Oscar Reyes <>
2016-11-16 21:37:19 -0600

    Added variable declarations and constructor to CourseIdentity class

--- src/scheduleCreator/model/CourseIdentity.java
+++ src/scheduleCreator/model/CourseIdentity.java
@@ -1,5 +1,17 @@
 package scheduleCreator.model;
 
-public class CourseIdentity {
-
+public class CourseIdentity 
+{
+	private final String courseDepartment;
+	private final String courseNumber;
+	private final String courseName;
+	
+	public CourseIdentity(final String department, final String number,
+           final String name)
+	{
+		this.courseDepartment = department;
+		this.courseNumber = number;
+		this.courseName = name;
+	}
+	
 }
