05d2a4932b84a5efea43d7c0c1175106b7a6b510
Oscar Reyes <>
2016-11-18 21:04:05 -0600

    added creditHourse variable to CollegeCourse Class

--- src/scheduleCreator/model/CollegeCourse.java
+++ src/scheduleCreator/model/CollegeCourse.java
@@ -2,9 +2,11 @@ package scheduleCreator.model;
 
 public class CollegeCourse {
 	private final CourseIdentity courseInfo;
+	private final int creditHours;
 	
 	public CollegeCourse(final CourseIdentity identity){
 		this.courseInfo = identity;
+		this.creditHours = identity.getCourseNumber().charAt(1);
 	}
 	
 }
