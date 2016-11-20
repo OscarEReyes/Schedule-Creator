9b73c11881dc81d158769dda190600c144ed7ae9
Oscar Reyes <>
2016-11-20 13:04:20 -0600

    Reworked the builder pattern implementation in CourseInstance Class. Removed reference to a CourseIdentity variable courseInfo

--- src/scheduleCreator/model/CourseInstance.java
+++ src/scheduleCreator/model/CourseInstance.java
@@ -6,54 +6,57 @@ public class CourseInstance extends CollegeCourse {
 	private final String classCrn;
 	private final String classSection;
 	private final String classProf;
-	private final CourseIdentity courseInfo;
 	
 	//optional
 	private CourseInstance lab;
 	
 	public CourseInstance(CourseInstanceBuilder builder){
-		super(builder.courseInfo);
 		this.schedule = builder.schedule;
 		this.placesLeft = builder.placesLeft;
 		this.classCrn = builder.classCrn;
 		this.classSection = builder.classSection;
 		this.classProf = builder.classProf;
 		this.lab = builder.lab;
-		this.courseInfo = builder.courseInfo;
 	}
 	
 	public static class CourseInstanceBuilder{
-			private final Schedule schedule;
-			private final int placesLeft;
-			private final String classCrn;
-			private final String classSection;
-			private final String classProf;
+			private Schedule schedule;
+			private int placesLeft;
+			private String classCrn;
+			private String classSection;
+			private String classProf;
 			private CourseInstance lab;
-			private final CourseIdentity courseInfo;
 			
-			public CourseInstanceBuilder(
-					final Schedule schedule,
-					final int placesLeft,
-					final String classCrn,
-					final String classSection,
-					final String classProf,
-					final CourseIdentity courseInfo){	
-			this.schedule = schedule;
-			this.placesLeft = placesLeft;
-			this.classCrn = classCrn;
-			this.classSection = classSection;
-			this.classProf = classProf;
-			this.courseInfo = courseInfo;
-	}
+			public CourseInstanceBuilder(final Schedule schedule){	
+				this.schedule = schedule;
+			}	
 			
+			public CourseInstanceBuilder placesLeft(int placesLeft){
+				this.placesLeft = placesLeft;
+				return this;
+			}
+			public CourseInstanceBuilder classCrn(String classCrn){
+				this.classCrn = classCrn;
+				return this;
+			}
 	
-		public CourseInstanceBuilder lab(CourseInstance lab){
-			this.lab = lab;
-			return this;
-		}
+			public CourseInstanceBuilder classSection(String classSection){
+				this.classSection = classSection;
+				return this;
+			}
+			
+			public CourseInstanceBuilder classProf(String classProf){
+				this.classProf = classProf;
+				return this;
+			}
 		
-		public CourseInstance build(){
-			return new CourseInstance(this);
-		}
+			public CourseInstanceBuilder lab(CourseInstance lab){
+				this.lab = lab;
+				return this;
+			}
+			
+			public CourseInstance build(){
+				return new CourseInstance(this);
+			}
 	}
 }
