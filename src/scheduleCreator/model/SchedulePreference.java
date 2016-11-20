80c19da8e2061d8fca239783380e47bf9acb735a
Oscar Reyes <>
2016-11-19 22:53:08 -0600

    Created setters and getters for SchedulePreference Class. Abbreviated Preferences in variable names, changed constructor to have default values for all variables

--- src/scheduleCreator/model/SchedulePreference.java
+++ src/scheduleCreator/model/SchedulePreference.java
@@ -6,22 +6,51 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 
 public class SchedulePreference {
-	private final StringProperty hoursPreference;
-	private final StringProperty daysPreference;
-	private final BooleanProperty breaksPreference;
-	private final StringProperty beginEndPreference;
-	
-	// Default Constructor
-	
+	private final StringProperty hoursPref;
+	private final StringProperty daysPref;
+	private final BooleanProperty breaksPref;
+	private final StringProperty beginEndPref;
+		
 	 public SchedulePreference() {
-	        this(null, null, null, null);
+	        this.hoursPref = new SimpleStringProperty("any");
+	        this.daysPref = new SimpleStringProperty("all");
+	        this.breaksPref = new SimpleBooleanProperty(false);
+	        this.beginEndPref = new SimpleStringProperty("end");
 	 }
-	
-	 public SchedulePreference(String hoursPreference, String daysPreference,
-			 Boolean breaksPreference, String beginEndPreference) {
-	        this.hoursPreference = new SimpleStringProperty(hoursPreference);
-	        this.daysPreference = new SimpleStringProperty(daysPreference);
-	        this.breaksPreference = new SimpleBooleanProperty(breaksPreference);
-	        this.beginEndPreference = new SimpleStringProperty(beginEndPreference);
-	    }
+	 
+	 // Getter Methods
+	 
+	 public String getHoursPref(){
+		 return hoursPref.get();
+	 }
+	 
+	 public String getDaysPref(){
+		 return daysPref.get();
+	 }
+	 
+	 public Boolean getBreaksPref(){
+		 return breaksPref.get();
+	 }
+	 
+	 public String getBeginEndPref(){
+		 return beginEndPref.get();
+	 }
+	 
+	 // Setter Methods
+	 
+	 public void setHoursPref(String hoursPref) {
+	        this.hoursPref.set(hoursPref);
+	 }
+	 
+	 public void setDaysPref(String daysPref) {
+	        this.daysPref.set(daysPref);
+	 }
+	 
+	 public void setBreaksPref(Boolean breaksPref) {
+	        this.breaksPref.set(breaksPref);
+	 }
+	 
+	 public void setBeginEndPref(String beginEndPref) {
+	        this.beginEndPref.set(beginEndPref);
+	 }
 }
