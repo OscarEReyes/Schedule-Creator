ab5e815644cba5b20481b6ad58c0b6337c7b2479
Oscar Reyes <>
2016-11-20 20:08:39 -0600

    Added an ObservableList of CollegeCourse objects that will be manipulated by MainApp and a method to retrieve that ObservableList

--- src/scheduleCreator/MainApp.java
+++ src/scheduleCreator/MainApp.java
@@ -3,17 +3,37 @@ package scheduleCreator;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import scheduleCreator.model.CollegeCourse;
 
 public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+    
+    private ObservableList<CollegeCourse> CollegeCourseData = FXCollections.observableArrayList();
 
+    /**
+     * Constructor
+     */
+    public MainApp() {
+
+    }
+
+    /**
+     * Returns the data as an observable list of CollegeCourses. 
+     * @return
+     */
+    public ObservableList<CollegeCourse> getCollegeCourseData() {
+        return CollegeCourseData;
+    }
+    
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
