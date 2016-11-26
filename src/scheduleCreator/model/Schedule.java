bebbf1dbaa24a391a7b14f2676d2f4cc149b3282
Oscar Reyes <>
2016-11-26 12:24:45 -0600

    Changed bracket to reflect 'egyptian brackets' convention

--- src/scheduleCreator/model/Schedule.java
+++ src/scheduleCreator/model/Schedule.java
@@ -1,30 +1,25 @@
 package scheduleCreator.model;
 
-public class Schedule 
-{
+public class Schedule {
 	private final String classDays;
 	private final String classHours;
 	private final String classLocation;
 	
-	public Schedule(final String days, final String hours, final String location)
-	{
+	public Schedule(final String days, final String hours, final String location) {
 		this.classDays = days;
 		this.classHours = hours;
 		this.classLocation = location;
 	}
 	
-	public String getClassDays()
-	{
+	public String getClassDays() {
 		return this.classDays;
 	}
 	
-	public String getClassHours()
-	{
+	public String getClassHours() {
 		return this.classHours;
 	}
 	
-	public String getClassLocation()
-	{
+	public String getClassLocation() {
 		return this.classLocation;
 	}
 	
