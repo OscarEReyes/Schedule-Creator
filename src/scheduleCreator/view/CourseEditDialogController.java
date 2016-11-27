61a1085a10e4a40af404ffbbe7ac4da7340c2d3d
Oscar Reyes <>
2016-11-27 11:54:40 -0600

    Added Variable declarations

--- src/scheduleCreator/view/CourseEditDialogController.java
+++ src/scheduleCreator/view/CourseEditDialogController.java
@@ -1,5 +1,29 @@
 package scheduleCreator.view;
+import java.awt.TextField;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import scheduleCreator.model.CollegeCourse;
+
+
+/*
+ * Controller for CourseEditDialog
+ * Edits CollegeCourse data.
+ */
 public class CourseEditDialogController {
-
+	@FXML
+	private TextField courseDepartmentField;
+	
+	@FXML
+	private TextField courseNumberField;
+	
+	@FXML 
+	private TextField courseNameField;
+	
+	@FXML
+	private TextField prefProfField;
+	
+	private Stage dialogStage;
+	private CollegeCourse course;
+	private Boolean confirmClicked = false;
 }
