211f16656d33bbb9167f343c54d9a78f7ada05a3
Oscar Reyes <>
2016-11-28 01:08:59 -0600

    Added getUser Method

--- src/scheduleCreator/view/LoginDialogController.java
+++ src/scheduleCreator/view/LoginDialogController.java
@@ -18,22 +18,10 @@ public class LoginDialogController {
 	private User user;
 	private Boolean loginClicked = false;
 	
-	public class User{
-		private final String username;
-		private final String password;
-		
-		public User(final String username, final String password){
-			this.username = username;
-			this.password = password;
-		}
-		
-		public String getUsername(){
-			return this.username;
-		}
-		
-		public String getPassword(){
-			return this.password;
-		}
+	
+	
+	public User getUser() {
+		return this.user;
 	}
 	
 	/**
@@ -118,5 +106,25 @@ public class LoginDialogController {
             return false;
         }
     }
+    
+    public class User{
+		private final String username;
+		private final String password;
+		
+		public User(final String username, final String password){
+			this.username = username;
+			this.password = password;
+		}
+		
+		public String getUsername() {
+			return this.username;
+		}
+		
+		public String getPassword() {
+			return this.password;
+		}
+		
+		
+	}
 
 }
