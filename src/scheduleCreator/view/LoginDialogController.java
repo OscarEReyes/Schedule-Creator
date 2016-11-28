f80d930c2f4f3abad07898e0a3c99d90e22f8dda
Oscar Reyes <>
2016-11-28 01:23:19 -0600

    Removed useless import

--- src/scheduleCreator/view/LoginDialogController.java
+++ src/scheduleCreator/view/LoginDialogController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-import scheduleCreator.model.CollegeCourse;
 
 public class LoginDialogController {
 	@FXML
