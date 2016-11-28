846c520088939dc311b6e2906cfab17a1e177dac
Oscar Reyes <>
2016-11-27 21:59:52 -0600

    Added some login functionality and cancel handling

--- src/scheduleCreator/view/LoginDialogController.java
+++ src/scheduleCreator/view/LoginDialogController.java
@@ -1,17 +1,17 @@
 package scheduleCreator.view;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.StringProperty;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-import scheduleCreator.model.CollegeCourse;
 
 public class LoginDialogController {
 	@FXML
-	private TextField userName;
+	private TextField usernameField;
 	@FXML
-	private TextField password;
+	private TextField passwordField;
 	
 	private Stage dialogStage;
 	private Boolean loginClicked = false;
@@ -33,4 +33,79 @@ public class LoginDialogController {
 			return this.password;
 		}
 	}
+	
+	/**
+     * Initializes the controller class. 
+     * Automatically called once the fxml file has loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+
+    
+    /**
+     * Sets this dialog's stage.
+     * 
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+    
+    
+    /**
+     * Executed if the user clicks confirm.
+     */
+    @FXML
+    private void handleLogin() {
+        if (isInputValid()) {
+        	User user = new User(usernameField.getText(), passwordField.getText());
+            loginClicked = true;
+            dialogStage.close();
+        }
+    }
+    
+    /**
+     * Executed when the cancel button is clicked.
+     */
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+    
+    
+    /**
+     * Verifies that the user input is valid.
+     * 
+     * @return true when input is valid.
+     */
+    private boolean isInputValid() {
+        String errorMessage = "";
+
+        if (usernameField.getText() == null) {
+            errorMessage += "Not a valid username!\n"; 
+        }
+        
+        if (passwordField.getText() == null || 
+        		passwordField.getText().length() != 6) {
+            errorMessage += "Not a valid 6-character pin!\n"; 
+        }
+
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Create error message window.
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Invalid Fields");
+            alert.setContentText(errorMessage);
+            
+            // Show error message window and wait for a response.
+            alert.showAndWait();
+
+            return false;
+        }
+    }
+
 }
