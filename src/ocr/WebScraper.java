656c9bc80becd8b483d9b276edbf6c20eb6b8cdf
Oscar Reyes <>
2016-11-29 17:48:07 -0600

    Removed unnecessary imports

--- src/ocr/WebScraper.java
+++ src/ocr/WebScraper.java
@@ -2,27 +2,22 @@ package ocr;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.Dimension;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 import org.openqa.selenium.support.ui.Select;
 
 import scheduleCreator.model.CollegeCourse;
-import scheduleCreator.view.LoginDialogController.User;
-import scheduleCreator.view.SemesterDialogController.Semester;
+
 
 public class WebScraper {
 
