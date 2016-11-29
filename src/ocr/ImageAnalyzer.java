d62721f2ad8040273837e0292fa8a2fe4776a56b
Oscar Reyes <>
2016-11-29 17:22:50 -0600

    Add analyzeImage method

--- src/ocr/ImageAnalyzer.java
+++ src/ocr/ImageAnalyzer.java
@@ -1,5 +1,23 @@
 package ocr;
 
-public class ImageAnalyzer {
+import java.io.File;
 
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class ImageAnalyzer {
+	
+	public String analyzeImage(File imageFile){
+		ITesseract instance = new Tesseract();
+
+		try {
+			instance.setDatapath("C:\\Users\\oscarjr\\Documents\\Programming\\workspace\\scheduleCreator\\tessdata");
+			String result = instance.doOCR(imageFile);
+			return result;
+		} catch (TesseractException e){
+			System.err.println(e.getMessage());
+			return null;
+		}
+	}
 }
