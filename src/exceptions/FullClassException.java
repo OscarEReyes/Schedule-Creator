3203eca39b843368504a8b1b5fa68fe65ee8e72e
Oscar Reyes <>
2016-12-20 17:58:44 -0600

    Added Documentation

--- src/exceptions/FullClassException.java
+++ src/exceptions/FullClassException.java
@@ -1,11 +1,17 @@
 package exceptions;
 
+/**
+ * Custom Exception class for cases where a CourseClass object has no spaces left.
+ * In cases such as the aforementioned, this exception will be thrown.
+ * 
+ * @author Oscar Reyes
+ *
+ */
 public class FullClassException extends Exception{
 	
   public FullClassException() {}
 
-  public FullClassException(String message)
-  {
+  public FullClassException(String message) {
      super(message);
 
   }
--- src/exceptions/InvalidSectionException.java
+++ src/exceptions/InvalidSectionException.java
@@ -1,5 +1,25 @@
 package exceptions;
 
-public class InvalidSectionException {
+/**
+ * Custom Exception class for cases where the section of a Course Class
+ * Object is not that of 3 Digits. For other cases, this exception will
+ * be thrown.
+ * 
+ * The reason for this is that Three-lettered section codes or two-lettered
+ * codes followed by a number are given to Dual-enrollment classes as well
+ * as honors college classes. None of these classes are of importance
+ * when looking for classes to create a schedule.
+ * 
+ * 
+ * @author Oscar Reyes
+ *
+ */
+public class InvalidSectionException extends Exception{
+	 
+	public InvalidSectionException() {}
+
+	  public InvalidSectionException(String message) {
+	     super(message);
+	  }
 
 }
