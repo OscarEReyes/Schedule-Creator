9146720b5fa848bab6b17cb53dd4baf64e837682
Oscar Reyes <>
2017-11-09 08:13:12 -0600

    Made tesseract datapath a variable.

--- src/ocr/ImageAnalyzer.java
+++ src/ocr/ImageAnalyzer.java
@@ -7,18 +7,17 @@ import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 public class ImageAnalyzer {
-	
-	public String analyzeImage(File imageFile){
+	final static String TESSDATA_DATAPATH = "C:\\Users\\oscarjr\\Documents\\Programming\\workspace\\scheduleCreator\\tessdata";
+	public static String analyzeImage(File imageFile){
 		ITesseract instance = new Tesseract();
-
 		try {
-			instance.setDatapath("C:\\Users\\oscarjr\\Documents\\Programming\\workspace\\scheduleCreator\\tessdata");
+			instance.setDatapath(TESSDATA_DATAPATH);
 			String result = instance.doOCR(imageFile);
 			System.out.println(result);
 			return result;
 		} catch (TesseractException e){
 			System.err.println(e.getMessage());
-			return null;
+			return "";
 		}
 	}
 }
